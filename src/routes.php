60090965503c5b842ebac4fea3208fd170764a08
Rick Kuipers <rick93@gmail.com>
2015-02-09 21:49:03 +0100

    Remove order transition route

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,6 @@ namespace Application;
 
 use Application\Model\Order;
 use Silex\Application;
-use SM\StateMachine\StateMachineInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /** @var Application $app */
@@ -17,17 +16,7 @@ $app->get('/', function (Application $app) {
 $order = $app['session']->get('order');
 
 $app->get('/order', function (Application $app) use ($order) {
-    /** @var StateMachineInterface $stateMachine */
-    $stateMachine = $app['statemachine.factory']->get($order);
     return $app['twig']->render('order.html.twig', [
         'order' => $order,
-        'stateMachine' => $stateMachine,
     ]);
 })->bind('order');
-
-$app->get('/order/{transition}', function (Application $app, $transition) use ($order) {
-    /** @var StateMachineInterface $stateMachine */
-    $stateMachine = $app['statemachine.factory']->get($order);
-    $stateMachine->apply($transition);
-    return new RedirectResponse($app['url_generator']->generate('order'));
-})->bind('order/transition');
